c55214d0cbf5cb4871d4667621587a063a799b6f
Meng-Ling-Ori <82038493+Meng-Ling-Ori@users.noreply.github.com>
2021-04-18 16:57:22 +0200

    Update value_free_environment.py

--- value_free_environment.py
+++ value_free_environment.py
@@ -40,42 +40,42 @@ class environment_free_operant(object):
         
         self.nm = nm
         
-        self.reinforcers = np.zeros((trials,nm-1), dtype = int)
+        self.reinforcers = np.zeros((trials,nm), dtype = int)
         self.effort_rate = np.zeros(trials)
         self.rewards = np.zeros((trials))
     
     def obtained_reinforcement(self,t,action,VR,VI,omission):
-        
+        reinforcer = np.zeros(self.nm) # the number of obtained reinforcer: leisure, pellets and effort
         if VR != None: # for VR schedule
             if not omission:
-                if action[0] == 1: #if not press, reinforcer = non-rewards
-                    reinforcer = 0
-                    number_of_reinforcer = 1
-                else: # if press, 10% obtained 'pellets'
-                    reinforcer = 1
-                    number_of_reinforcer = sum([np.random.choice(range(self.nm-1), p = [1-VR/100, VR/100]) for i in range(np.where(action ==1)[0][0])])
+                if action[0] != 1: # if press, 10% obtained 'pellets'
+                    reinforcer[1] = sum([np.random.choice(range(self.nm-1), p = [1-VR/100, VR/100]) for i in range(np.where(action ==1)[0][0])])
+                else: # if not press, 100% get 'leisure', 
+                    reinforcer[0] = 1
+                reinforcer[2] = self.effort_rate[t]/ 60  # effort in one second           
+                    
             else:
                 if action[0] == 1: #if press, reinforcer = non-rewards
-                    reinforcer = 0
                     number_of_reinforcer = 1
                 else: # if press, 10% obtained 'pellets'
-                    reinforcer = 1
                     number_of_reinforcer = sum([np.random.choice(range(self.nm-1), p = [1-VR/100, VR/100]) for i in range(np.where(action ==1)[0][0])])                
+
         
-        else: # for VI schedule
-            if action[0] == 1: # if not press, reinforcer = non-rewards
-                reinforcer = 0
-            elif max(self.reinforcers[1]) == 0 : # if press and haven't got pellets , get 'pellets'
-                reinforcer = 1
-            elif t - np.where(self.reinforcers[1] == 1)[0][-1] >= VI: # if press but already got 'pellets' in previous trial, and previous six trial did not get 'pellets', get 'pellets'
-                reinforcer = 1
-            else:
-                reinforcer = 0
-            number_of_reinforcer = 1
+#        else: # for VI schedule
+#            if action[0] == 1: # if not press, reinforcer = non-rewards
+#                reinforcer = 0
+#            elif max(self.reinforcers[1]) == 0 : # if press and haven't got pellets , get 'pellets'
+#                reinforcer = 1
+#            elif t - np.where(self.reinforcers[1] == 1)[0][-1] >= VI: # if press but already got 'pellets' in previous trial, and previous six trial did not get 'pellets', get 'pellets'
+#                reinforcer = 1
+#            else:
+#                reinforcer = 0
+#            number_of_reinforcer = 1
             
-        self.reinforcers[t,reinforcer] = number_of_reinforcer
+#        self.reinforcers[t,reinforcer] = number_of_reinforcer
+        self.reinforcers[t] = reinforcer
         
-        return reinforcer,number_of_reinforcer
+        return reinforcer
     
     def obtained_effort(self,t,action_rate):            
         ### obtained effort                        
@@ -87,10 +87,3 @@ class environment_free_operant(object):
     def obtained_rewrads(self,t,U):
         ### obtained rewards
         self.rewards[t] = U[1]*self.reinforcers[t,1] + U[2]*self.effort_rate[t]
-            
-        
-        
-        
-        
-        
-        
